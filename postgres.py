620c24aeb25d1c9e4ddcb8c838d8ca58206e36ad
sashcallaysilva <sashcallaysilva@gmail.com>
2025-01-21 21:15:30 -0500

    main: Added files manually (Sebastian)

---
+++ postgres.py
@@ -0,0 +1,19 @@
+# DOWNLOADING POSTGRES INTO LOCAL PC
+# MODULE: PSYCOPG2
+
+import psycopg2
+import config as cfg
+
+try:
+    print("Start...")
+    engine = psycopg2.connect(database="initial_db", host=cfg.db_host, port=cfg.db_port, user=cfg.db_user, password=cfg.db_password)
+    print("Trying...")
+    if engine.status == psycopg2.extensions.STATUS_READY:
+        print("Connection is open and ready.")
+    else:
+        print("Connection is not ready")
+
+    engine.close()
+
+except psycopg2.Error as e:
+    print("Error connecting to db: ", e)
