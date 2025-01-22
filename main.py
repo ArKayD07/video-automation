dc833473c01881cffd0185b566dd7872392d7b15
ArKayD07 <76012241+ArKayD07@users.noreply.github.com>
2025-01-21 20:07:11 -0500

    code progress 1/21/2025

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import openai
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("path/to/your/credentials.json", scope)
+client = gspread.authorize(creds)
+
+spreadsheet = client.open("Video Automation System")
+sheet = spreadsheet.sheet1
+first_cell_value = sheet.cell(1, 1).value
+
+print(f"The value of the first cell is: {first_cell_value}")
