6921497cb15568dafc3714bc27b589932821c822
ArKayD07 <76012241+ArKayD07@users.noreply.github.com>
2025-01-27 19:52:21 -0500

    Update main.py - Kristiyan

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import openai
 import edge_tts
 import asyncio
+import requests
+import configuration
+import json
 
 file_path = 'gptPrompt.txt'
 with open(file_path, 'r') as file:
@@ -21,20 +24,49 @@ def generate_audio(text, audio_name="output.mp3"):
     try:
         voice = "en-GB-SoniaNeural"
         communicate = edge_tts.Communicate(text, voice)
-        asyncio.run(communicate.save(audio_name))
+        asyncio.run(communicate.save(f"Video Files/{audio_name}"))
     except Exception as e:
         print(f"An error occurred: {e}")
 
-question = input('Enter a question: ')
-response = chatbot(prompt, question)
+def generate_image(prompt, image_name):
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    image_data = requests.get(image_url).content
+    with open(image_name, 'wb') as handler:
+        handler.write(image_data)
 
-chapters = response.split("\n\n")
+category = input('Enter a category: ')
+videoType = input('Enter a video type: ')
+userInput = f"The video category is: {category}. The video type is: {videoType}."
+response = chatbot(prompt, userInput)
+
+print("Response from chatbot:\n", response)
+
 chapter_strings = []
-for chapter in chapters:
-    chapter_strings.append(chapter.replace(chapter[chapter.find('*'):chapter.find('\n')], "").replace('\n', ''))
+image_prompts = []
+
+try:
+    response_json = json.loads(response)
+    for chapter in response_json['chapters']:
+        chapter_strings.append(chapter['secondary text'])
+        image_prompts.extend(chapter['image prompts'])
+except json.JSONDecodeError as e:
+    print(f"JSON decoding failed: {e}")
+    exit(1)
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+    exit(1)
 
 for i in range(len(chapter_strings)):
     audio_name = 'audio-' + str(i+1) + ".mp3"
-    generate_audio(chapter_strings[i],audio_name)
+    generate_audio(chapter_strings[i], audio_name)
 
-#test
+for i, prompt in enumerate(image_prompts):
+    image_name = f'Video Files/image-{i+1}.png'
+    generate_image(prompt, image_name)
+
+print(image_prompts)
