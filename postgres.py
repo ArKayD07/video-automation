3796df8b7805e350d74e86a26e76f70623945939
sashcallaysilva <sashcallaysilva@gmail.com>
2025-01-27 19:03:00 -0500

    main: postgres.py functions

--- postgres.py
+++ postgres.py
@@ -4,7 +4,8 @@
 import psycopg2
 import configuration as cfg
 
-
+# db_checkStatus(): Checks the connection status with the AWS RDS
+#                   Returns 1 if status is READY, otherwise returns 0.
 def db_checkStatus():
     try:
         conn = psycopg2.connect(database=cfg.db_database, host=cfg.db_host, port=cfg.db_port, user=cfg.db_user, password=cfg.db_password)
@@ -20,7 +21,7 @@ def db_checkStatus():
     except psycopg2.Error as e:
         print(f"Error connecting to db: {e}")
 
-
+# db_exec(): Executes a SQL statement and commits changes to database in AWS 
 def db_exec(msg):
     try:
         conn = psycopg2.connect(database=cfg.db_database, host=cfg.db_host, port=cfg.db_port, user=cfg.db_user, password=cfg.db_password)
@@ -28,16 +29,45 @@ def db_exec(msg):
         cursor.execute(msg)
         conn.commit() # should commit changes to db 
         conn.close()
+        return 1
 
     except psycopg2.Error as e:
         print(f"Error connecting to db: {e}")
+        return 0
 
-table_name = "tabletest"
-test_msg = f''' 
-            CREATE TABLE IF NOT EXISTS {table_name}(
-                id INTEGER PRIMARY KEY NOT NULL,
-                text_field TEXT 
-            )
-            '''
+# db_createTable: Executes a CREATE TABLE command. 
+#                 The arguments for this function are:
+#                 (1) the name of the table >> string
+#                 (2) a list of parameters >> list of (string, string list) tuples
+#
+#             Ex: db_createTable("test_table", [("id", ["PRIMARY KEY", "INTEGER"])])
+
+def db_createTable(tableName,params):
+    cols = ''''''
+    n = len(params)
+    for i in range(n):
+        # param[0]: name of column
+        # param[1]: list of keywords
+        if i != n - 1:
+            cols += f'''{params[i][0]} {" ".join(params[i][1])},'''
+        else:
+            cols += f'''{params[i][0]} {" ".join(params[i][1])}'''
+
+
+    msg = f'''CREATE TABLE IF NOT EXISTS {tableName}({cols})'''
+    db_exec(msg)
+
+# db_dropTable: Executes a DROP TABLE command.
+#               The argument for this function is the table name
+#
+#           Ex: db_dropTable("test_table") >> drops test_table from DB
+
+def db_dropTable(tableName):
+    msg = f'''DROP TABLE {tableName}'''
+    db_exec(msg)
+
+
+
+
 
 
